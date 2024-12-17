f1ce4d20bff09560f34a7c6fabfe002bb46069c8
shantocnits <shanto.cnits@gmail.com>
2024-12-17 17:27:48 +0600

    cls 1

---
+++ class-1/app.js
@@ -0,0 +1,41 @@
+// // Class 1-4  variable deccleare............
+// var x=10;
+// var y=20;
+// var z=x+y;
+// document.write(z);
+// // Class 1-4 variable deccleare............
+
+// // Class 5 primitive data type...........
+// // String,number,boolean,undefined,null
+// var a="Raki";
+// document.write(a)
+
+// var b="10";
+// document.write(b)
+
+// var c;
+// document.write(c)
+
+// var d=null;
+// document.write(d)
+
+// var e=true;
+// document.write(e)
+// // Class 5 ...........
+
+// Class 6 non primitive data type...........
+// object,array,regexp
+// object..
+var a={
+    Name: "Shan",
+    Age:30,
+    City:"Pab",
+}
+document.write(a.City)
+
+// Array..
+var b=["pab","raj","dhaka"]
+
+document.write(b);
+
+// Class 6 non primitive data type...........
